70deee1b276a7115e16eb4d6e2803e610a074ace
u-droid <u.tyagi2013@gmail.com>
2023-09-02 15:43:15 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path = os.path.join('artifacts', 'train.csv')
+    test_data_path = os.path.join('artifacts','test.csv')
+    raw_data_path = os.path.join('artifacts','data.csv')
+
+class DataIngestion:
+    def __init__(self) -> None:
+        self.ingestion_config = DataIngestionConfig()
+    
+    def initiate_data_ingestion(self):
+        logging.info('Entered Data Ingestion')
+        try:
+            df = pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the data as dataframe')
+            os.makedirs( os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, header=True, index=False)
+            
+            logging.info('Train Test Split Initiated')
+            train, test = train_test_split(df, test_size=0.2, random_state=42)
+            train.to_csv(self.ingestion_config.train_data_path, header=True, index=False)
+            test.to_csv(self.ingestion_config.test_data_path, header=True, index=False)
+            logging.info('Ingestion of data is completed')
+            return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
+        except Exception as e:
+            CustomException(e, sys)
+
+if __name__ == '__main__':
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
+
+
